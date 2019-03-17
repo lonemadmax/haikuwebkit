e5b5ba39caad4d3e3c50eb2935f08c013bc23b4d
RAJAGOPALAN-GANGADHARAN <g.raju2000@gmail.com>
2019-03-17 08:40:40 +0000

    Configuring minibrowser build and execute [early stage]

--- Tools/Scripts/webkitdirs.pm
+++ Tools/Scripts/webkitdirs.pm
@@ -3196,6 +3196,10 @@ sub runMiniBrowser
         my $webKitLauncherPath = File::Spec->catfile(executableProductDir(), "MiniBrowser.exe");
         return system { $webKitLauncherPath } $webKitLauncherPath, @ARGV;
     }
+    if(isHaiku()){
+       my $webKitLauncherPath = File::Spec->catfile(productDir(), "MiniBrowser");
+       return system {  $webKitLauncherPath } $webKitLauncherPath, @ARGV;
+    }
     return 1;
 }
 
