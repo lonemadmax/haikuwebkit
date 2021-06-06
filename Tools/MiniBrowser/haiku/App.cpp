haiku/webkit

Added message handler to main run loop and made changes to webview structure

Tools/MiniBrowser/haiku/App.cpp
@@ -4,20 +4,13 @@
  */
 
 
-#include "App.h"
-//#include "BrowserWindow.h"
+
 #include <Window.h>
 #include <View.h>
 
-#include "WKPageConfigurationRef.h"
-#include "WKPage.h"
-#include "WKView.h"
-#include "WKURL.h"
-#include "WKString.h"
-#include "WKContext.h"
-#include "WKPreferencesRef.h"
-//test import
-//#include "WebView.h"
+#include "App.h"
+
+
 
 enum
 {
@@ -31,27 +24,12 @@ App::App(void)
   fCount = 0;
 
   //create window
-  BRect frame(100,100,500,400);
+  frame.Set(100,100,500,400);
   myWindow = new BWindow(frame,"MiniBrowser"
                                  , B_TITLED_WINDOW
                                  , B_ASYNCHRONOUS_CONTROLS
                                    | B_QUIT_ON_WINDOW_CLOSE);
-  //webkit stuff
-  auto config = adoptWK(WKPageConfigurationCreate());
-  auto prefs = WKPreferencesCreate();
-  
-  
-  WKPreferencesSetDeveloperExtrasEnabled(prefs, true);
-  WKPageConfigurationSetPreferences(config.get(),prefs);
-  
-  fContext = adoptWK(WKContextCreateWithConfiguration(nullptr));
-  //fprintf(stderr,"here");
-  WKPageConfigurationSetContext(config.get(),fContext.get());
-  
-  fViewPort=adoptWK(WKViewCreate("Webkit",frame,myWindow,config.get()));
-  //
   myWindow->Show();
-  this->testLoader();
 }
 
 void
@@ -59,11 +37,6 @@ App::MessageReceived(BMessage *msg)
 {
   switch(msg->what)
   {
-    case M_BUTTON_GO_URL:
-    {
-      testLoader();
-      break;
-    }
     default:
     {
       BApplication::MessageReceived(msg);
@@ -71,18 +44,15 @@ App::MessageReceived(BMessage *msg)
     }
   }
 }
-
+void App::ReadyToRun()
+{
+	webView = new WebView(frame,myWindow);
+	this->testLoader();
+}
 void 
 App::testLoader()
 {
-	fprintf(stderr,"\nim loading");
-	auto page = WKViewGetPage( fViewPort.get());
-	WKRetainPtr<WKURLRef> uri;
-	uri = adoptWK(WKURLCreateWithUTF8CString("about:blank"));
-	WKRetainPtr<WKStringRef> str;
-	str = adoptWK(WKStringCreateWithUTF8CString("<body>Hello world</body>"));
-	//WKPageLoadURL(page,uri.get());
-	WKPageLoadHTMLString(page,str.get(),uri.get()); 
+	 webView->loadHTML();
 }
 
 int

lonemadmax/haikuwebkit

Tools/MiniBrowser/haiku/App.cpp
@@ -4,20 +4,13 @@
  */
 
 
-#include "App.h"
-//#include "BrowserWindow.h"
+
 #include <Window.h>
 #include <View.h>
 
-#include "WKPageConfigurationRef.h"
-#include "WKPage.h"
-#include "WKView.h"
-#include "WKURL.h"
-#include "WKString.h"
-#include "WKContext.h"
-#include "WKPreferencesRef.h"
-//test import
-//#include "WebView.h"
+#include "App.h"
+
+
 
 enum
 {
@@ -31,27 +24,12 @@ App::App(void)
   fCount = 0;
 
   //create window
-  BRect frame(100,100,500,400);
+  frame.Set(100,100,500,400);
   myWindow = new BWindow(frame,"MiniBrowser"
                                  , B_TITLED_WINDOW
                                  , B_ASYNCHRONOUS_CONTROLS
                                    | B_QUIT_ON_WINDOW_CLOSE);
-  //webkit stuff
-  auto config = adoptWK(WKPageConfigurationCreate());
-  auto prefs = WKPreferencesCreate();
-  
-  
-  WKPreferencesSetDeveloperExtrasEnabled(prefs, true);
-  WKPageConfigurationSetPreferences(config.get(),prefs);
-  
-  fContext = adoptWK(WKContextCreateWithConfiguration(nullptr));
-  //fprintf(stderr,"here");
-  WKPageConfigurationSetContext(config.get(),fContext.get());
-  
-  fViewPort=adoptWK(WKViewCreate("Webkit",frame,myWindow,config.get()));
-  //
   myWindow->Show();
-  this->testLoader();
 }
 
 void
@@ -59,11 +37,6 @@ App::MessageReceived(BMessage *msg)
 {
   switch(msg->what)
   {
-    case M_BUTTON_GO_URL:
-    {
-      testLoader();
-      break;
-    }
     default:
     {
       BApplication::MessageReceived(msg);
@@ -71,18 +44,15 @@ App::MessageReceived(BMessage *msg)
     }
   }
 }
-
+void App::ReadyToRun()
+{
+	webView = new BWebView(frame,myWindow);
+	this->testLoader();
+}
 void 
 App::testLoader()
 {
-	fprintf(stderr,"\nim loading");
-	auto page = WKViewGetPage( fViewPort.get());
-	WKRetainPtr<WKURLRef> uri;
-	uri = adoptWK(WKURLCreateWithUTF8CString("about:blank"));
-	WKRetainPtr<WKStringRef> str;
-	str = adoptWK(WKStringCreateWithUTF8CString("<body>Hello world</body>"));
-	//WKPageLoadURL(page,uri.get());
-	WKPageLoadHTMLString(page,str.get(),uri.get()); 
+	 webView->loadHTML();
 }
 
 int