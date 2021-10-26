2d90e00328739cfb6d6f66df92bf1b0dce38beeb
Adrien Destugues <pulkomandy@pulkomandy.tk>
2021-10-26 11:19:45 +0200

    Revert "Haiku: Make gigacage use Overcommit by default"
    This reverts commit 44871cb0e724924eb5128ada6aa2c8dacd8b42b8.
    
    Causes crash on Amazon and probably other places. And it was a change in
    platform-shared code which is usually a bad idea (the WebKit people know
    what they are doing, most of the time).
    
    Fixes #17351.

--- Source/WTF/wtf/Gigacage.cpp
+++ Source/WTF/wtf/Gigacage.cpp
@@ -20,7 +20,7 @@
  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
  * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
 #include "config.h"
@@ -48,7 +48,7 @@ void* tryAllocateZeroedVirtualPages(Kind, size_t requestedSize)
 {
     size_t size = roundUpToMultipleOf(WTF::pageSize(), requestedSize);
     RELEASE_ASSERT(size >= requestedSize);
-    void* result = OSAllocator::reserveUncommitted(size);
+    void* result = OSAllocator::reserveAndCommit(size);
 #if ASSERT_ENABLED
     if (result) {
         for (size_t i = 0; i < size / sizeof(uintptr_t); ++i)
